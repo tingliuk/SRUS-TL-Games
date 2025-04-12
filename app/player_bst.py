9aba6e57dd60cc9ea35685ce18acee0504b2cc81
Teck Poo Tan <china.liuting@163.com>
2025-04-12 14:32:16 +0800

    test_player_bst.py added

--- app/player_bst.py
+++ app/player_bst.py
@@ -1,6 +1,27 @@
+from player_bnode import PlayerBNode
 class PlayerBST:
     def __init__(self):
         self._root=None
     @property
     def root(self):
         return self._root
+
+    def insert(self,player):
+        if self._root is None:
+            self._root=PlayerBNode(player)
+        else:
+            self._insert_recursive(self._root,player)
+
+    def _insert_recursive(self,current_node,player):
+        if player.name<current_node.player.name:
+            if current_node.left is None:
+                current_node.left=PlayerBNode(player)
+            else:
+                self._insert_recursive(current_node.left,player)
+        elif player.name > current_node.player.name:
+            if  current_node.right is None:
+                current_node.right=PlayerBNode(player)
+            else:
+                self._insert_recursive(current_node.right,player)
+        else:
+            current_node.player=player #update duplicate player
---
+++ test/test_player_bst.py
@@ -0,0 +1,32 @@
+import unittest
+from player_bst import PlayerBST
+from player_bnode import PlayerBNode
+from player import Player
+class TestPlayerBST(unittest.TestCase):
+    def setUp(self):
+        self.tree=PlayerBST()
+        self.p1=Player("Bob",101,10)
+        self.p2=Player("Kathy",102,20)
+        self.p3 = Player("Amy",102,20)
+        self.p4 = Player("Bob",101,20) # duplicate player
+
+    def test_insert_single_node(self):
+        self.tree.insert(self.p1)
+        self.assertEqual(self.tree.root.player.name,"Bob")
+
+    def test_insert_multiple_nodes(self):
+        self.tree.insert(self.p1)
+        self.tree.insert(self.p2)
+        self.tree.insert(self.p3)
+        root=self.tree.root
+        self.assertEqual(root.player.name,"Bob")# set Bob as root
+        self.assertEqual(root.left.player.name,"Amy")# left child less than root
+        self.assertEqual(root.right.player.name, "Kathy")# right child greater than root
+
+    def test_insert_duplicate_player(self):
+
+        self.tree.insert(self.p1)
+        self.tree.insert(self.p4)# inserted a duplicate player
+        root=self.tree.root
+        self.assertEqual(root.player.score, 20)# check if root has been updated to new score
+
